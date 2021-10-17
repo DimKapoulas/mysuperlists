fd3bb0419eb34ceb936ff8e6de19680047a7bdb1
Dimitris Kapoulas <dimkapoulas@hotmail.com>
2021-10-17 15:35:21 +0300

    Created one to one relationship for Item and List. Modified tests to create a List before Item creation

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# The list holding specific user's to-do items
+class List(models.Model):
+    pass
+
+# The to-do item itself
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(
+        List,
+        on_delete=models.CASCADE,
+        default=None)
+
+
