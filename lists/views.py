1ffe5804a92d9ed4b1b536f4de8a3f312714b371
Dimitris Kapoulas <dimkapoulas@hotmail.com>
2021-10-17 17:53:17 +0300

    new URL and view for adding to existing lists. FT passes

--- lists/views.py
+++ lists/views.py
@@ -10,13 +10,57 @@ def home_page(request):
     
 
 # View for specific list
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list': list_})
 
 
 # View for creating new list
 def new_list(request):
+    """Creates new list for user and redirects to it
+
+    Parameters
+    ----------
+    request: Request
+        The incoming request
+
+
+    Returns
+    -------
+    redirections to created list
+    """
+
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_.id}/')
+
+# View for creating a new item
+def add_item(request, list_id):
+    """
+    Creates new item for selected list and redirects
+    to corresponding list view
+
+    Parameters
+    ----------
+    request: Request
+        The incoming request
+    list_id: int
+        The id of related To-Do list
+    
+    Returns
+    -------
+
+    Raises
+    ------
+    ValueEror
+    """
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
+
+    
+
+    
+
--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,7 @@ from lists import views
 urlpatterns = [
     url('^$', views.home_page, name='home'),
     url('^lists/new$', views.new_list, name='new_list'),
-    url('^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url('^lists/(\d+)/$', views.view_list, name='view_list'),
+    url('^lists/(\d+)/add_item$', views.add_item, name='view_list'),
 ]
+    
