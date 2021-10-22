b11dc48c7556c1b76b0ea34496d3a737002f8760
Dimitris Kapoulas <dimkapoulas@hotmail.com>
2021-10-23 00:28:49 +0300

    Minor Fixes

---
+++ example.py
@@ -0,0 +1,3 @@
+names = "name1,name2,name3"
+parsed = names.split(',')
+print(parsed)
--- superlists/settings.py
+++ superlists/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+# DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
